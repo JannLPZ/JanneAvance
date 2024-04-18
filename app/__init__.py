51678a8ac01f9dc5d3ec285a52a2450cabe38637
JannLPZ <20213tn147@utez.edu.mx>
2024-04-18 14:35:58 -0600

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_wtf.csrf import CSRFProtect
+
+app = Flask(__name__)
+
+csrf=CSRFProtect()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=(['GET','POST']))
+def login():
+    """
+    print(request.method)
+    print(request.form['usuario'])
+    print(request.form['password'])
+    """
+    #csfr (cross-site Request forgery):solicitud de falsificacion entre sitios
+    if request.method=='POST':
+       #print(request.form['usuario'])
+       #print(request.form['password'])
+        if request.form['usuario'] == 'admin1' and request.form['password']=='123456':
+            return redirect(url_for('index'))
+        else:
+            return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
+    
+def pagina_no_encontrada(error):
+    return render_template('errores/404.html'), 404
+
+
+def inicializar_app(config):
+    app.config.from_object(config)
+    csrf.init_app(app)
+    app.register_error_handler(404, pagina_no_encontrada)
+    return app
